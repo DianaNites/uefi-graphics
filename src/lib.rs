eaa2b78229a7c558f03ac6d7323c36e67d61baaf
Diana <5275194+DianaNites@users.noreply.github.com>
2020-08-11 16:07:01 -0400

    Testing

--- src/lib.rs
+++ src/lib.rs
@@ -71,3 +71,62 @@ impl<'a, T: Into<Bgr888> + PixelColor> DrawTarget<T> for UefiDisplay<'a> {
         )
     }
 }
+
+/// Same as `UefiDisplay` but without a generic DrawTarget.
+///
+/// Testing purposes only.
+pub struct UefiDisplayNotGeneric<'a> {
+    info: ModeInfo,
+    fb: FrameBuffer<'a>,
+}
+
+impl<'a> UefiDisplayNotGeneric<'a> {
+    /// Create a new [`UefiDisplay`].
+    pub fn new(info: ModeInfo, fb: FrameBuffer<'a>) -> Self {
+        Self { info, fb }
+    }
+}
+
+impl<'a> DrawTarget<Bgr888> for UefiDisplayNotGeneric<'a> {
+    type Error = Unsupported;
+
+    fn draw_pixel(&mut self, item: Pixel<Bgr888>) -> Result<(), Self::Error> {
+        let Pixel(point, color) = item;
+        let Size { width, height } = self.size();
+        let stride: u64 = self
+            .info
+            .stride()
+            .try_into()
+            .expect("Stride didn't fit in u64. Buggy UEFI firmware?");
+        let (x, y) = (point.x as u64, point.y as u64);
+        if x < width.into() && y < height.into() {
+            let index: usize = ((y * stride + x) * 4)
+                .try_into()
+                .expect("Framebuffer index overflowed usize");
+            unsafe {
+                match self.info.pixel_format() {
+                    PixelFormat::RGB => {
+                        self.fb.write_value(index, Rgb888::from(color));
+                    }
+                    PixelFormat::BGR => {
+                        self.fb.write_value(index, color);
+                    }
+                    _ => return Err(Unsupported(())),
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn size(&self) -> Size {
+        let (width, height) = self.info.resolution();
+        Size::new(
+            width
+                .try_into()
+                .expect("Display width didn't fit in u32. Buggy UEFI firmware?"),
+            height
+                .try_into()
+                .expect("Display height didn't fit in u32. Buggy UEFI firmware?"),
+        )
+    }
+}

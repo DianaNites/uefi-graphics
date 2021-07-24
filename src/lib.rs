de13fd934b295416ccb7b66c79de7f7aeb5f8ae6
Diana <5275194+DianaNites@users.noreply.github.com>
2021-07-23 21:35:43 -0400

    Don't depend on the uefi crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,25 @@
 //! An embedded-graphics display driver for UEFI environments
 #![no_std]
-use core::convert::TryInto;
+use core::{convert::TryInto, marker::PhantomData};
 use embedded_graphics::{drawable::Pixel, pixelcolor::*, prelude::*, DrawTarget};
-use uefi::proto::console::gop::*;
 
 #[derive(Debug)]
 pub struct Unsupported(());
+
+impl Unsupported {
+    fn new<T>(_: T) -> Self {
+        Unsupported(())
+    }
+}
+
+/// Pixel format to use
+pub enum PixelFormat {
+    /// Red Green Blue
+    Rgb,
+
+    /// Blue Green Red
+    Bgr,
+}
 
 /// UEFI Display driver.
 ///
@@ -16,22 +30,37 @@ pub struct Unsupported(());
 /// At the moment this display only supports the BGR888 and RGB888 UEFI pixel
 /// formats. BltOnly and Bitmask are unsupported.
 pub struct UefiDisplay<'a> {
-    info: ModeInfo,
-    fb: FrameBuffer<'a>,
+    /// UEFI Framebuffer
+    fb: *mut u8,
+    pixel: PixelFormat,
+    stride: u32,
+    size: (u32, u32),
+    spooky: PhantomData<&'a mut [u8]>,
 }
 
 impl<'a> UefiDisplay<'a> {
     /// Create a new [`UefiDisplay`].
-    pub fn new(info: ModeInfo, fb: FrameBuffer<'a>) -> Self {
-        Self { info, fb }
+    ///
+    /// `fb` must be the UEFI framebuffer base, `pixel` the pixel format in use,
+    /// `stride` the pixel stride,
+    /// and `size` the horizontal and vertical resolution, respectively.
+    ///
+    /// In the UEFI spec this information is found
+    /// in the `EFI_GRAPHICS_OUTPUT_MODE_INFORMATION` structure.
+    pub fn new(fb: *mut u8, pixel: PixelFormat, stride: u32, size: (u32, u32)) -> Self {
+        Self {
+            fb,
+            pixel,
+            stride,
+            size,
+            spooky: PhantomData,
+        }
     }
 
     /// Return the size of the display
     pub fn size(&self) -> Size {
-        let (width, height) = self.info.resolution();
-        // Shouldn't ever fail, both for reasonable limits and because
-        // ModeInfo::resolution casts to usize from u32 for some reason..
-        Size::new(width.try_into().unwrap(), height.try_into().unwrap())
+        // Size::new(self.size.0, self.size.1)
+        Size::from(self.size)
     }
 }
 
@@ -41,7 +70,7 @@ impl<'a, T: Into<Bgr888> + PixelColor> DrawTarget<T> for UefiDisplay<'a> {
     fn draw_pixel(&mut self, item: Pixel<T>) -> Result<(), Self::Error> {
         let Pixel(point, color) = item;
         let mut bytes = [0u8; 3];
-        match self.info.pixel_format() {
+        match self.pixel {
             PixelFormat::Rgb => {
                 bytes
                     .copy_from_slice(&Rgb888::from(color.into()).into_storage().to_be_bytes()[1..]);
@@ -49,17 +78,15 @@ impl<'a, T: Into<Bgr888> + PixelColor> DrawTarget<T> for UefiDisplay<'a> {
             PixelFormat::Bgr => {
                 bytes.copy_from_slice(&color.into().into_storage().to_be_bytes()[1..]);
             }
-            _ => return Err(Unsupported(())),
         }
         let Size { width, height } = <Self as DrawTarget<T>>::size(self);
-        // ModeInfo::stride goes from u32 to usize, so this can't fail.
-        let stride: u64 = self.info.stride().try_into().unwrap();
+        let stride: u64 = self.stride.into();
         let (x, y) = (point.x as u64, point.y as u64);
         if x < width.into() && y < height.into() {
             let index: usize = (((y * stride) + x) * 4)
                 .try_into()
-                .expect("Framebuffer index overflowed usize");
-            unsafe { self.fb.write_value(index, bytes) };
+                .map_err(Unsupported::new)?;
+            unsafe { (self.fb.add(index) as *mut [u8; 3]).write_volatile(bytes) };
         }
         Ok(())
     }
